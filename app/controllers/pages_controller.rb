f212a383793ca2f9fe34384af8b3b2d6fb967e76
Bruce Makaaru <brucemakallan@gmail.com>
2018-03-24 01:24:10 +0300

    Heroku Mail Test 5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
   
   def home
     @carousel_items = {
