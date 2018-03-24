a60d5027c705413525497a88883561872e4dce78
Bruce Makaaru <brucemakallan@gmail.com>
2018-03-24 19:27:03 +0300

    Form Emails styling

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -13,12 +13,3 @@ class FormMailer < ApplicationMailer
 		mail(to: "makallancg2@gmail.com", subject: "Sheevys Feedback")
 	end
 end
-
-# body: "Full name: #{email.fullname}
-# 			Phone #{email.phone}
-# 			Check-in Date #{email.datein}
-# 			Check-out Date #{email.dateout}
-# 			Room-Type #{email.roomtype}
-# 			Occupancy #{email.occupancy}
-# 			Adults #{email.adults}
-# 			Children #{email.children}")
