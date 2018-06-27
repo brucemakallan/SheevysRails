b933fcda1c0ea6a598321e8b406ba33537c723f5
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-27 14:42:53 +0300

    Testing mail with personal address

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -5,11 +5,11 @@ class FormMailer < ApplicationMailer
 
 	def send_email(email)
 		@email = email
-		mail(to: "doreen@eatforlife.ug", subject: "New Sheevys Booking")			
+		mail(to: "brucemakallan@gmail.com", subject: "New Sheevys Booking")			
 	end
 
 	def send_feedback(feedback)
 		@feedback = feedback
-		mail(to: "doreen@eatforlife.ug", subject: "Sheevys Feedback")
+		mail(to: "brucemakallan@gmail.com", subject: "Sheevys Feedback")
 	end
 end
