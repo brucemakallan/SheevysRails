d81e581f5df33eab61ce3f5ed2e55653d693e805
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-04-17 17:06:05 +0300

    Test with one email

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -5,17 +5,17 @@ class FormMailer < ApplicationMailer
 
 	def send_email(email)
 		@email = email
-		mail(to: "tinamugarra@sheevys.com", subject: "New Sheevys Booking")	
+		# mail(to: "tinamugarra@sheevys.com", subject: "New Sheevys Booking")	
 		mail(to: "caarinaitwe97@gmail.com", subject: "New Sheevys Booking")
 		# sending to webmaster for record keeping and debugging		
-		mail(to: "brucemakallan@gmail.com", subject: "New Sheevys Booking")			
+		# mail(to: "brucemakallan@gmail.com", subject: "New Sheevys Booking")			
 	end
 
 	def send_feedback(feedback)
 		@feedback = feedback
-		mail(to: "tinamugarra@sheevys.com", subject: "Sheevys Feedback")
+		# mail(to: "tinamugarra@sheevys.com", subject: "Sheevys Feedback")
 		mail(to: "caarinaitwe97@gmail.com", subject: "Sheevys Feedback")
 		# sending to webmaster for record keeping and debugging
-		mail(to: "brucemakallan@gmail.com", subject: "Sheevys Feedback")
+		# mail(to: "brucemakallan@gmail.com", subject: "Sheevys Feedback")
 	end
 end
