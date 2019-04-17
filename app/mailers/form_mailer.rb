077f26c0ab4cd170275dec6a4307b90615887953
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-04-17 16:59:53 +0300

    Add other admin email

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -6,6 +6,7 @@ class FormMailer < ApplicationMailer
 	def send_email(email)
 		@email = email
 		mail(to: "tinamugarra@sheevys.com", subject: "New Sheevys Booking")	
+		mail(to: "caarinaitwe97@gmail.com", subject: "New Sheevys Booking")
 		# sending to webmaster for record keeping and debugging		
 		mail(to: "brucemakallan@gmail.com", subject: "New Sheevys Booking")			
 	end
@@ -13,6 +14,7 @@ class FormMailer < ApplicationMailer
 	def send_feedback(feedback)
 		@feedback = feedback
 		mail(to: "tinamugarra@sheevys.com", subject: "Sheevys Feedback")
+		mail(to: "caarinaitwe97@gmail.com", subject: "Sheevys Feedback")
 		# sending to webmaster for record keeping and debugging
 		mail(to: "brucemakallan@gmail.com", subject: "Sheevys Feedback")
 	end
