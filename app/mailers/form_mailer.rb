50fa58d95fb418f022dfa9832439c078644599d7
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-04-17 12:40:55 +0300

    Restore client email and add Webmaster email for debugging

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -5,13 +5,15 @@ class FormMailer < ApplicationMailer
 
 	def send_email(email)
 		@email = email
-		# mail(to: "tinamugarra@sheevys.com", subject: "New Sheevys Booking")			
+		mail(to: "tinamugarra@sheevys.com", subject: "New Sheevys Booking")	
+		# sending to webmaster for record keeping and debugging		
 		mail(to: "brucemakallan@gmail.com", subject: "New Sheevys Booking")			
 	end
 
 	def send_feedback(feedback)
 		@feedback = feedback
-		# mail(to: "tinamugarra@sheevys.com", subject: "Sheevys Feedback")
+		mail(to: "tinamugarra@sheevys.com", subject: "Sheevys Feedback")
+		# sending to webmaster for record keeping and debugging
 		mail(to: "brucemakallan@gmail.com", subject: "Sheevys Feedback")
 	end
 end
